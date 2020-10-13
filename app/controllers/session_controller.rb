21b8f066dedf5cea4f22acfd3289f274685b14f6
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-13 00:13:39 -0400

    attempts to fix error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,7 @@ class SessionController < ApplicationController
     def login
         user = User.find_by(email: params[:email])
         if user
-            password = params[:password]
+            password = params[:password_digest]
             is_authenticated = user.authenticate(password)
             if is_authenticated
                 render json: { token: encoded_token(user_payload(user)), current_user: user}
