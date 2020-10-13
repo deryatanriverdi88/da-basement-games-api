8481b66dce87beaf0c5bd377620442dff1ef0f0b
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-13 16:40:53 -0400

    updates login method within session_controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,7 @@ class SessionController < ApplicationController
     def login
         user = User.find_by(email: params[:email])
         if user
-            password = params[:password_digest]
+            password = params[:password]
             is_authenticated = user.authenticate(password)
             if is_authenticated
                 render json: { token: encoded_token(user_payload(user)), current_user: user}
@@ -10,7 +10,7 @@ class SessionController < ApplicationController
                 render json: {error: "INCORRECT INFIORMATION, PLEASE TRY AGAIN"}
             end
         else
-            render json: {error: errors.full_messages}
+            render json: {error: "INCORRECT INFIORMATION, PLEASE TRY AGAIN"}
         end
     end
 end
