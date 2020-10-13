4dcc5e48bfeef80e23a3146e7f4eee8c9110c389
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-13 00:41:30 -0400

    changes params for favorite_cards

--- app/controllers/favorite_cards_controller.rb
+++ app/controllers/favorite_cards_controller.rb
@@ -32,7 +32,7 @@ class FavoriteCardsController < ApplicationController
     private
 
     def favorite_card_params
-        params.permit(:user_id, :magic_the_gatherig_card_id, :amount, :foil, :normal)
+        params.permit(:user_id, :amount, :foil, :normal, :name, :img_url, :category_id, :product_id, :group_id, :rarity, :sub_type, :text, :group_name, :normal_low_price , :normal_mid_price, :normal_high_price, :normal_market_price, :foil_low_price, :foil_mid_price, :foil_high_price,:foil_market_price)
     end
 
     def update_params
