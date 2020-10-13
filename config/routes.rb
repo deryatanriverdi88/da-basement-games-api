0164dfcebdf16f32f13f125b3302f7f600d0b1e7
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-12 22:17:14 -0400

    adds routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :favorite_cards
   resources :users
+
+  post "/login", to: "sessions#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
