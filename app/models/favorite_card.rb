a33618b2c178c90eeb925f216d97262c49dfc3f1
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-12 22:04:09 -0400

    sets validations for FavoriteCard

--- app/models/favorite_card.rb
+++ app/models/favorite_card.rb
@@ -1,2 +1,7 @@
 class FavoriteCard < ApplicationRecord
+    belongs_to :user
+
+    validates :amount, presence: true
+    validates :amount, numericality: {other_than: 0}
+    validates :user, presence: true
 end
