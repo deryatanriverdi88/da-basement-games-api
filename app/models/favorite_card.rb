65df2aeab7eb87d4eed5d3bd37aedaf25b20ab0b
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-20 05:39:39 -0400

    adds self.default_order method to favorite_cards model

--- app/models/favorite_card.rb
+++ app/models/favorite_card.rb
@@ -5,6 +5,10 @@ class FavoriteCard < ApplicationRecord
     validates :amount, numericality: {other_than: 0}
     validates :user, presence: true
 
+    def self.default_order
+        order('id ASC')
+    end
+
     def self.alphabetical
         order(:name)
     end
