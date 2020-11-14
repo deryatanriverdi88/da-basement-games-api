b4cc50a1b5769d586d990a83a47e3499dbbcf466
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-13 21:47:47 -0500

    adds a route for cards_with_binder

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/profile', to: 'users#profile'
   get '/cards', to: 'favorite_cards#cards', as: 'cards'
+  get '/cards_with_binder', to: 'favorite_cards#cards_with_binder', as: 'cards_with_binder'
   get '/binder_item', to: 'binders#binder_item', as: 'binder_item'
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
