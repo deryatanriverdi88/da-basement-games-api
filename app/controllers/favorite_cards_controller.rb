5b6ea5fb06d1056f2da176531a437d395f896030
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-27 20:55:27 -0500

    updates name of cards_with_setname to card_with_setname

--- app/controllers/favorite_cards_controller.rb
+++ app/controllers/favorite_cards_controller.rb
@@ -50,7 +50,7 @@ class FavoriteCardsController < ApplicationController
         render json: favorite_cards
     end
 
-    def cards_with_setname
+    def card_with_setname
         favorite_card = []
         render json: favorite_card = FavoriteCard.all.filter{|card| card[:name] == params[:name] && card[:group_name] == params[:setName]}
     end
