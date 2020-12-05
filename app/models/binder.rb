f7bf7949c8bdc6196f74c9ca5e94a515cc9d0847
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-12-04 19:55:30 -0500

    adds new validations to Binder

--- app/models/binder.rb
+++ app/models/binder.rb
@@ -2,6 +2,8 @@ class Binder < ApplicationRecord
   belongs_to :user
   has_many :favorite_cards, dependent: :destroy
 
+  validate :private, presence: true
+
   def self.alphabetical
     order(:name)
   end
