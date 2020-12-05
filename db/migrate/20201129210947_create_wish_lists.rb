29d38bf214f2b38af9e5a08a6eb12caac43d6380
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-12-04 21:05:32 -0500

    adds 20201129210947_create_wish_lists

---
+++ db/migrate/20201129210947_create_wish_lists.rb
@@ -0,0 +1,11 @@
+class CreateWishLists < ActiveRecord::Migration[6.0]
+    def change
+      create_table :wish_lists do |t|
+        t.belongs_to :user, null: false, foreign_key: true
+        t.belongs_to :favorite_card, null: false, foreign_key: true
+        t.integer :amount
+  
+        t.timestamps
+      end
+    end
+  end
